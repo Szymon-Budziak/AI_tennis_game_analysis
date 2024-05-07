d36a18872ad2638c23a5351d7b5f8a1ceb34fd1d
Szymon-Budziak <szymonbudziak@gmail.com>
2024-05-07 18:59:17 +0200

    chore: added new utils for bbox calculations

--- src/tennis_ai/utils/__init__.py
+++ src/tennis_ai/utils/__init__.py
@@ -1,3 +1,5 @@
 from .video_utils import read_video, save_video
-from .bbox_utils import get_center_of_bbox, measure_distance
+
+from .bbox_utils import (get_center_of_bbox, measure_distance, get_foot_position, get_closest_keypoint_index,
+                         get_height_of_bbox, measure_xy_distance)
 from .conversions import convert_pixels_to_meters_distances, convert_meters_to_pixels_distances
--- src/tennis_ai/utils/bbox_utils.py
+++ src/tennis_ai/utils/bbox_utils.py
@@ -1,4 +1,5 @@
-__all__ = ['get_center_of_bbox', 'measure_distance']
+__all__ = ['get_center_of_bbox', 'measure_distance', 'get_foot_position', 'get_closest_keypoint_index',
+           'get_height_of_bbox', 'measure_xy_distance']
 
 
 def get_center_of_bbox(bbox):
@@ -8,3 +9,30 @@ def get_center_of_bbox(bbox):
 
 def measure_distance(p1, p2):
     return ((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2) ** 0.5
+
+
+def get_foot_position(bbox):
+    x1, y1, x2, y2 = bbox
+    return int((x1 + x2) / 2), y2
+
+
+def get_closest_keypoint_index(point, keypoints, keypoint_indices):
+    closest_distance = float('inf')
+    key_point_index = keypoint_indices[0]
+    for key_point_idx in keypoint_indices:
+        key_point = (keypoints[key_point_idx * 2], keypoints[key_point_idx * 2 + 1])
+        distance = abs(point[1] - key_point[1])
+
+        if distance < closest_distance:
+            closest_distance = distance
+            key_point_index = key_point_idx
+
+    return key_point_index
+
+
+def get_height_of_bbox(bbox):
+    return bbox[3] - bbox[1]
+
+
+def measure_xy_distance(p1, p2):
+    return abs(p1[0] - p2[0]), abs(p1[1] - p2[1])
